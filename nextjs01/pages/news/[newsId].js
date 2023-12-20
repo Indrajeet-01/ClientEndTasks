d022287daaf5b7c66955e66f182a2f37643b8143
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2023-12-20 16:22:31 +0530

    dynamic paging

--- nextjs01/pages/news/[newsId].js
+++ nextjs01/pages/news/[newsId].js
@@ -1,4 +1,8 @@
+import {useRouter} from 'next/router'
+
 function detailedNews() {
+    const router = useRouter()
+    const newsId = router.query.newsId
     return  <h1>
         this is detailed page of news
     </h1>
